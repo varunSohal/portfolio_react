e349f4c4ff97442f8ade85180a44916a8f84c616
Varundeep06 <139569049+Varundeep06@users.noreply.github.com>
2024-04-09 18:56:54 -0500

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:varunsohal18@email.com">varunsohal18@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/varunSohal">github.com/varunSohal</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
